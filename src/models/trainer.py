974b279ddc5dd2655d05771746e865d8953e56f8
retajjjj <retagmohamed2004@gmail.com>
2024-12-27 20:11:00 +0200

    error in plan requests

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -12,5 +12,25 @@ class Trainer(BaseUser):
     def __init__(self, email: str, first_name: str, last_name: str, *args, **kwargs):
         super().__init__(email, first_name, last_name, *args, **kwargs)
 
+    
+    def get_pending_requests_by_trainer(self):
+        requests = [
+            r
+            for r in workout_request.WorkoutRequest.get_requests_by_trainer(self.id)
+            if r.status == Status.Pending
+        ]
+        return requests
+        
+    def reject_plan_request(self, plan_id:int) -> None:
+        if request := workout_request.WorkoutRequest.get(plan_id):
+            print(request)
+            request.update_status(Status.Rejected)
 
+    def accept_plan_request(self, plan_id:int) -> None:
+        if request := workout_request.WorkoutRequest.get(plan_id):
+            request.update_status(Status.Accepted)
+
+    
+
+        
 
